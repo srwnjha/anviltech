a28a92e2e5f369b1b3776032ade0020b350947e4
Pramit Amatya <pramitamatya786@gmail.com>
2026-02-19 13:18:00 +0545

    nothing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Toaster } from 'sonner';
-import Home from '@/pages/Home';
-import About from '@/pages/About';
-import Services from '@/pages/Services';
-import Portfolio from '@/pages/Portfolio';
-import Blog from '@/pages/Blog';
-import BlogDetail from '@/pages/BlogDetail';
-import Testimonials from '@/pages/Testimonials';
-import Contact from '@/pages/Contact';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "sonner";
+import Home from "@/pages/Home";
+import About from "@/pages/About";
+import Services from "@/pages/Services";
+import Portfolio from "@/pages/Portfolio";
+import Blog from "@/pages/Blog";
+import BlogDetail from "@/pages/BlogDetail";
+import Testimonials from "@/pages/Testimonials";
+import Contact from "@/pages/Contact";
 
 function App() {
   return (
     <Router>
-      <Toaster 
-        position="top-right" 
-        richColors 
+      <Toaster
+        position="top-right"
+        richColors
         closeButton
         toastOptions={{
           style: {
-            fontFamily: 'Inter, sans-serif',
+            fontFamily: "Inter, sans-serif",
           },
         }}
       />
